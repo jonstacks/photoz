1f4f32b4c983d35418ac166c4e630b7f8379cac1
Jonathan Stacks <Jonathan.Stacks@Cerner.com>
2015-10-20 09:52:30 -0500

    Add command to list bucket contents

---
+++ photos/management/commands/list_bucket.py
@@ -0,0 +1,52 @@
+"""
+Command to list the bucket contents.
+
+Make sure to have the following set in the django settings file:
+
+* AWS_ACCESS_KEY_ID
+* AWS_SECRET_ACCESS_KEY
+* AWS_STORAGE_BUCKET_NAME
+* AWS_S3_HOST
+
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+import boto
+import boto.s3.connection
+from prettytable import PrettyTable
+
+
+class Command(BaseCommand):
+    help = 'List the contents of the static and media files bucket in S3.'
+
+    def bytes_to_kb(self, byts):
+        return float(byts)/ 1024
+
+    def handle(self, *args, **options):
+        conn = boto.connect_s3(
+            aws_access_key_id = settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
+            host = settings.AWS_S3_HOST,
+            calling_format = settings.AWS_S3_CALLING_FORMAT,
+        )
+
+        bucket = conn.lookup(settings.AWS_STORAGE_BUCKET_NAME)
+        if bucket is None:
+            raise CommandError('Bucket "%s" does not exist' %
+                settings.AWS_STORAGE_BUCKET_NAME)
+
+        table = PrettyTable([
+            "Key", "Last Modified", "Size (KB)", "Version ID"
+        ])
+        for key in bucket.list():
+            table.add_row([
+                key.name,
+                key.last_modified,
+                self.bytes_to_kb(key.size),
+                key.version_id,
+            ])
+
+        print table
+
